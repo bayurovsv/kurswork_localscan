16674d7251f65c3e3a1759aeee8151a3d8412412
bayurovsv <43810651+bayurovsv@users.noreply.github.com>
2019-04-10 17:09:22 +0300

    Add files via upload

---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(617, 24);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(260, 381);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(513, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 27);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Сканировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(409, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(204, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(305, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Имя компьютера:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(409, 29);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(173, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(343, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "IP-Адрес:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(455, 106);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            67000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(56, 20);
+            this.numericUpDown1.TabIndex = 6;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(453, 138);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            67000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(56, 20);
+            this.numericUpDown2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(305, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Начальное значение порта";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(305, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Конечное значение порта";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(426, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 27);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Искать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(693, 3);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Список IP-Адресов:";
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(542, 138);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            50000,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(56, 20);
+            this.numericUpDown3.TabIndex = 13;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(510, 108);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Таймаут ожидания";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(308, 72);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(111, 17);
+            this.checkBox1.TabIndex = 17;
+            this.checkBox1.Text = "На всех Адресах";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(8, 412);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(868, 23);
+            this.progressBar1.TabIndex = 18;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(308, 170);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(303, 235);
+            this.richTextBox1.TabIndex = 19;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(8, 22);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(291, 383);
+            this.richTextBox2.TabIndex = 20;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(885, 439);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Сканер локальной сети";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Runtime.Serialization;
+using System.Diagnostics;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public int StartPort;
+        public int EndPort;
+        public int timeO;
+        public int step = 1;
+        public Form1()
+        {
+            InitializeComponent();
+            listBox1.Items.Clear();
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timeO = Convert.ToInt32(numericUpDown3.Value);
+            try
+            {
+                AutoResetEvent waiter = new AutoResetEvent(false);
+                byte[] buffer = Encoding.ASCII.GetBytes("тестируем Ping");
+                string host = System.Net.Dns.GetHostName();
+                textBox1.Text = host;
+                List<string> serversList = new List<string>();
+                IPAddress ip = Dns.GetHostByName(host).AddressList[0];
+                textBox2.Text = ip.ToString();
+                string[] ipParts = ip.ToString().Split('.');
+                string[] ips = new string[254];
+                for (int i = 1; i < 255; i++)
+                {
+                    ips[i - 1] = ipParts[0] + '.' + ipParts[1] + '.' + ipParts[2] + '.' + i.ToString();
+                }
+                foreach (string ipToScan in ips)
+                {
+                    serversList.Add(ipToScan);
+                }
+                foreach (string server in serversList)
+                {
+                    Ping ping = new Ping();
+                    ping.PingCompleted += new PingCompletedEventHandler(p_PingCompleted);
+                    PingOptions options = new PingOptions(64, true);
+                    ping.SendAsync(IPAddress.Parse(server), timeO, buffer, options, waiter);
+                }
+                listBox1.Items.Clear();
+            }
+            catch (Exception)
+            {
+            }
+        }
+        private void p_PingCompleted(object sender, PingCompletedEventArgs e)
+        {
+            if (e.Reply.Status == IPStatus.Success)
+            {
+                listBox1.Items.Add(e.Reply.Address.ToString());
+            }
+            ((AutoResetEvent)e.UserState).Set();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                trrr_vse();
+                if (richTextBox2.Text == "")
+                    MessageBox.Show("Свободных портов не найдено!");
+            }
+            else
+            {
+                trrr();
+                if (richTextBox1.Text == "")
+                    MessageBox.Show("Свободных портов не найдено!");
+            }
+        }
+        public void trrr_vse()
+        {
+            richTextBox2.Text = null;
+            StartPort = Convert.ToInt32(numericUpDown1.Value);
+            EndPort = Convert.ToInt32(numericUpDown2.Value);
+            progressBar1.Minimum = StartPort;
+            progressBar1.Maximum = EndPort*listBox1.Items.Count+ listBox1.Items.Count;
+            progressBar1.Value = StartPort;
+            for (int CurrPort = StartPort; CurrPort <= EndPort; CurrPort++)
+            {
+                TcpClient TcpScan = new TcpClient();
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                {
+                    try
+                    {
+                        TcpScan.Connect(Convert.ToString(listBox1.Items[i]), CurrPort);
+                        richTextBox2.Text += "IP-Adress" + Convert.ToString(listBox1.Items[i]) + ": " + "Port " + Convert.ToString(CurrPort) + " Открыт\r\n" + Environment.NewLine;
+                    }
+                    catch
+                    {
+                    }
+                    progressBar1.Value += step;
+                }
+            }
+            progressBar1.Value = StartPort;
+        }
+        public void trrr()
+        {
+            richTextBox1.Text = null;
+            StartPort = Convert.ToInt32(numericUpDown1.Value);
+            EndPort = Convert.ToInt32(numericUpDown2.Value);
+            progressBar1.Minimum = StartPort;
+            progressBar1.Maximum = EndPort+1;
+            progressBar1.Value = StartPort;
+            for (int CurrPort = StartPort; CurrPort <= EndPort; CurrPort++)
+            { 
+                TcpClient TcpScan = new TcpClient();
+                try
+                {
+                    TcpScan.Connect(textBox2.Text, CurrPort);
+                    richTextBox1.Text += "Port " + Convert.ToString(CurrPort) + " Открыт\r\n" + Environment.NewLine;
+                }
+                catch
+                {          
+                }
+                progressBar1.Value += step;        
+            }  
+            progressBar1.Value = StartPort;
+        }
+
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
